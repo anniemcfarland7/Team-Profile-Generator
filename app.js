fd5521b0d5447351f19cca308fa103af70cf06c9
anniemcfarland7 <anniemcfarland@aol.com>
2021-01-29 14:21:20 -0600

    updating files

--- app.js
+++ app.js
@@ -7,29 +7,123 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
+const employees = []
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const manager = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the team manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the team manager's id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the team manager's e-mail?",
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the team manager's office number?",
+        },
+    ]).then((answers) => {
+        const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        employees.push(newManager)
+        promptUser();
+    })
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Which type of team member would you like to add?',
+            choices: ['Manager', 'Engineer', 'Intern', "I don't want to add anymore teammembers."],
+        }
+    ])
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+        .then((answers) => {
+            if (answers.role == "I don't want to add anymore teammembers.") {
+                createTeamProfile()
+            }
+            else if (answers.role == 'Manager') {
+                manager()
+            }
+            else if (answers.role == 'Engineer') {
+                engineer()
+            }
+            else if (answers.role == 'Intern') {
+                intern()
+            }
+        })
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+const engineer = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the engineer's id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's e-mail?",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's github username?",
+        },
+    ]).then((answers) => {
+        const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        employees.push(newEngineer)
+        promptUser()
+    })
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const intern = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the intern's id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the intern's e-mail?",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the intern's school?",
+        },
+    ]).then((answers) => {
+        const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school)
+        employees.push(newIntern)
+        promptUser()
+    })
+
+manager();
+
+function createTeamProfile() {
+    fs.writeFileSync(outputPath, render(employees))
+    console.log('Success! See your output folder for your new Team Profile html document.')
+}
